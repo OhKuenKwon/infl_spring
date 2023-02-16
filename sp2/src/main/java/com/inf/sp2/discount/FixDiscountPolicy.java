3a5e79b3dbdc37a1966011e85e03f4441df28cb7
OhKuenKwon <kok1027@naver.com>
2023-02-16 22:19:19 +0900

    디스카운트 정책을 위한 클래스

---
+++ sp2/src/main/java/com/inf/sp2/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package com.inf.sp2.discount;
+
+import com.inf.sp2.member.Grade;
+import com.inf.sp2.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+    private int discountFixAmount = 1000; //1000원 할인
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
