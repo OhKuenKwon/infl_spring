a718eb8542208032c11cf884b4d23c99a6dcdd99
OhKuenKwon <kok1027@naver.com>
2023-02-16 20:12:29 +0900

    테스트 클래스

---
+++ sp1/src/test/java/com/inf/sp1/member/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package com.inf.sp1.member;
+
+import com.inf.sp1.config.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig ac = new AppConfig();
+        memberService = ac.memberService();
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
