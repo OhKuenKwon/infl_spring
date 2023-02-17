a01097535652ef96b5e6d5bf8c6c2d728cf640c5
OhKuenKwon <kok1027@naver.com>
2023-02-17 21:48:51 +0900

    빈 설정 메타정보 테스트

--- sp2/src/test/java/com/inf/sp2/beandefinition/BeanDefinitionTest.java
+++ sp2/src/test/java/com/inf/sp2/beandefinition/BeanDefinitionTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+//빈 설정 메타정보 확인 테스트
 public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
     //GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
